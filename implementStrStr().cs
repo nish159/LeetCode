a312e417afa5fa9b0c02727c80bc1dae39449920
nish159 <77575013+nish159@users.noreply.github.com>
2022-04-28 21:09:56 -0400

    Create implementStrStr().cs

---
+++ implementStrStr().cs
@@ -0,0 +1,62 @@
+/*
+Implement strStr().
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+ 
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+*/
+
+namespace practice
+{
+    using System;
+    using System.Globalization;
+
+    public class Practice
+    {
+        static void Main(string[] args)
+        {
+            //string haystack = "hello";
+            //string needle = "ll";
+
+            string haystack = "aaaaa";
+            string needle = "bba";
+
+            StrStr(haystack, needle);
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+            if (haystack.Length == 0)
+            {
+                return 0;
+            }
+
+            int findNeedle = haystack.IndexOf(needle);
+
+            Console.WriteLine(findNeedle);
+            
+            return findNeedle;
+        }
+    }
+}
