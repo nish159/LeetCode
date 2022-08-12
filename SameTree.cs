ead6e64e25bb5dd37cbf5e4c42f5a63927f5520a
nish159 <77575013+nish159@users.noreply.github.com>
2022-08-12 12:58:54 -0400

    Update SameTree.cs

--- SameTree.cs
+++ SameTree.cs
@@ -20,7 +20,7 @@ The number of nodes in both trees is in the range [0, 100].
 -104 <= Node.val <= 104
 */
 
-/*
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     public int val;
