5e3b5f874e37782f2fc6a20d8e6bca36ccdfb4aa
nish159 <77575013+nish159@users.noreply.github.com>
2022-08-14 10:21:43 -0400

    Update ConcatenationOfArray.cs

--- ConcatenationOfArray.cs
+++ ConcatenationOfArray.cs
@@ -22,3 +22,16 @@ n == nums.length
 1 <= n <= 1000
 1 <= nums[i] <= 1000
 */
+
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int[] ans = new int[nums.Length * 2];
+        
+        for(int i = 0; i < nums.Length; i++) {
+            
+            ans[i] = nums[i];
+            ans[i + nums.Length] = nums[i];
+        }
+        return ans;
+    }
+}
