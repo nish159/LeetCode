2c1c42f3913b9feeff6b72305c033cc5b896ce75
nish159 <77575013+nish159@users.noreply.github.com>
2022-10-01 09:41:22 -0400

    Update HowManyNumberAreSmallerThanTheCurrentNumber.cs

--- HowManyNumberAreSmallerThanTheCurrentNumber.cs
+++ HowManyNumberAreSmallerThanTheCurrentNumber.cs
@@ -11,6 +11,7 @@ For nums[1]=1 does not exist any smaller number than it.
 For nums[2]=2 there exist one smaller number than it (1). 
 For nums[3]=2 there exist one smaller number than it (1). 
 For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
 Example 2:
 Input: nums = [6,5,4,8]
 Output: [2,1,0,3]
