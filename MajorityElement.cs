7a71eb6f5efab1e7d086b11fe78435cb43352398
nish404 <77575013+nish404@users.noreply.github.com>
2024-05-07 19:19:28 -0400

    Update MajorityElement.cs

--- MajorityElement.cs
+++ MajorityElement.cs
@@ -21,37 +21,38 @@ Follow-up: Could you solve the problem in linear time and in O(1) space?
 
 public class Solution {
     public int MajorityElement(int[] nums) {
-        // create a dictionary 
-        // key = number
-        // value = count 
-        
+        // Create a dictionary to store number-count pairs
         Dictionary<int, int> dictionary = new Dictionary<int, int>();
         
+        // Iterate through the array of numbers
         for (int i = 0; i < nums.Length; i++) {
-            // create a variable for the current number in the array
+            // Get the current number from the array
             var currNum = nums[i];
             
-            // if the dicitionary does not contain the current number add it with a value of 1
+            // If the dictionary does not contain the current number, add it with a count of 1
             if (!dictionary.ContainsKey(currNum)) {
                 dictionary.Add(currNum, 1);
             }
             else {
-                // increment the value by 1
+                // If the number is already in the dictionary, increment its count by 1
                 dictionary[currNum]++;
             }
         }
         
-        // we need to search for the max value within the key and the value
+        // Initialize variables to track the maximum key and value in the dictionary
         int maxKey = dictionary.First().Key;
         int maxValue = dictionary.First().Value;
         
+        // Iterate through the dictionary to find the key with the maximum value
         foreach (KeyValuePair<int, int> entry in dictionary) {
             if (entry.Value > maxValue) {
                 maxValue = entry.Value;
                 maxKey = entry.Key;
             }
         }
-        
+
+        // Return the key with the maximum value, which represents the majority element
         return maxKey;
     }
 }
+
