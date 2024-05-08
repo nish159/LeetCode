ab30dbac2748047800a33bdc7308cedef7909cb0
nish404 <77575013+nish404@users.noreply.github.com>
2024-05-08 17:13:04 -0400

    Update NON-REPEAT.cs

--- NON-REPEAT.cs
+++ NON-REPEAT.cs
@@ -29,3 +29,43 @@ public static char FirstNonRepeatingChar(string str)
 string inputString = "leetcode";
 char firstNonRepeating = FirstNonRepeatingChar(inputString);
 Console.WriteLine(firstNonRepeating); // Output: 'l'
+
+
+/*
+CONSTRAINTS:
+INPUT SHOULD NOT BE NULL 
+INPUT STRING CAN CONTAIN ANY INICODE CHARACTERS
+*/
+
+// Function to find the first non-repeating character
+public static char FirstNonRepeatingChar(string str)
+{
+    // Check if the input string is not null
+    if (str == null)
+    {
+        throw new ArgumentNullException(nameof(str), "Input string cannot be null.");
+    }
+
+    Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+    foreach (char c in str)
+    {
+        if (charCount.ContainsKey(c))
+            charCount[c]++;
+        else
+            charCount[c] = 1;
+    }
+
+    foreach (char c in str)
+    {
+        if (charCount[c] == 1)
+            return c;
+    }
+
+    return '\0'; // Return null character if no non-repeating character found
+}
+
+// Example usage
+string inputString = "leetcode";
+char firstNonRepeating = FirstNonRepeatingChar(inputString);
+Console.WriteLine(firstNonRepeating); // Output: 'l'
